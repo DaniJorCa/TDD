fef99f993a9ef333b07d52e00c3fbb03dfa56702
Dani <daniel.jorca@gmail.com>
2023-03-29 01:09:08 +0200

    Assertion + atributo velocidad

--- Coche.java
+++ Coche.java
@@ -1,2 +1,3 @@
 public class Coche {
+    public Object velocidad;
 }
--- TestCoche_Daniel_Jordan.java
+++ TestCoche_Daniel_Jordan.java
@@ -2,7 +2,8 @@ import org.junit.jupiter.api.Test;
 
 public class TestCoche_Daniel_Jordan {
     @Test
-    public void test_crear_coche_Daniel_Jordan(){
+    public void test_al_crear_coche_su_velocidad_es_cero_Daniel_Jordan(){
         Coche nuevoCoche = new Coche();
+        Assertions.asssertEquals(0,nuevoCoche.velocidad)
     }
 }
