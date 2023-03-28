254561f44aa2e84d55d8d2a92aca95dbd80c2da6
Dani <daniel.jorca@gmail.com>
2023-03-29 00:55:29 +0200

    Creamos Test JUnit

--- TestCoche_Daniel_Jordan.java
+++ TestCoche_Daniel_Jordan.java
@@ -1,3 +1,8 @@
-public class TestCoche_Daniel_Jordan {
+import org.junit.jupiter.api.Test;
 
+public class TestCoche_Daniel_Jordan {
+    @Test
+    public void test_crear_coche_Daniel_Jordan(){
+        Coche nuevoCoche = new Coche();
+    }
 }
