0de52c4b8c4529414113fb18e2af6c462009296c
Dani <daniel.jorca@gmail.com>
2023-03-29 18:57:27 +0200

    Creado nuevo test aceleracion y metodo acelerar

--- TestCoche_Daniel_Jordan.java
+++ TestCoche_Daniel_Jordan.java
@@ -2,8 +2,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 public class TestCoche_Daniel_Jordan {
     @Test
-    public void test_al_crear_coche_su_velocidad_es_cero_Daniel_Jordan(){
+    public void test_al_crear_coche_su_velocidad_es_cero_Daniel_Jordan() {
         Coche nuevoCoche = new Coche();
-        Assertions.assertEquals(0,nuevoCoche.velocidad);
+        Assertions.assertEquals(0, nuevoCoche.velocidad);
+    }
+    @Test
+    public void test_al_acelerar_coche_su_velocidad_aumenta_Daniel_Jordan(){
+         Coche nuevoCoche = new Coche();
+         nuevoCoche.acelerar(30);
+         Assertions.assertEquals(30,nuevoCoche.velocidad);
     }
 }
